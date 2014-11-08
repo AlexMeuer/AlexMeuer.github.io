95a28082678a26d42f26113d34f6655b687c3d99
Alex Meuer <alexmeuer@gmail.com>
2014-11-08 18:48:36 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Personal Website
+================
++ I threw this together in about two seconds with _Bootstrap_.
++ I _will_ be implementing python in the (hopefully, course permitting) near future.
---
+++ contactthanks.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Form Sent</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+		<h1>Your message has been sent!</h1><br />
+
+		<?php print "<meta http-equiv=\"refresh\" content=\"0;URL=contact.htm\">";
+		?>
+
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
